194c3799db63eeaad1329c65d05d8a1e795618cf
Michał Rola <michal.rola@circlekeurope.com>
2019-01-29 16:43:59 +0100

    Added missing kt file and made some changes: - added EncryptionObject - refactored code - removed comments

--- app/src/main/java/com/michalrola/fingerprintauth/FingerprintHandler.kt
+++ app/src/main/java/com/michalrola/fingerprintauth/FingerprintHandler.kt
@@ -6,53 +6,47 @@ import android.content.pm.PackageManager
 import android.hardware.fingerprint.FingerprintManager
 import android.os.CancellationSignal
 import android.support.v4.app.ActivityCompat
-import android.util.Log
 import android.widget.Toast
-import javax.crypto.Cipher
 
-class FingerprintHandler(context: Context): FingerprintManager.AuthenticationCallback() {
+class FingerprintHandler(private val context: Context) : FingerprintManager.AuthenticationCallback() {
 
     lateinit var cancellationSignal: CancellationSignal
-    var context: Context
 
-    init{
-        this.context = context
-    }
-
-    fun startAuth(fingerprintManager: FingerprintManager, cryptoObject: FingerprintManager.CryptoObject){
+    fun startAuth(fingerprintManager: FingerprintManager, cryptoObject: FingerprintManager.CryptoObject) {
         cancellationSignal = CancellationSignal()
 
-        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.USE_FINGERPRINT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             return
         }
         fingerprintManager.authenticate(cryptoObject, cancellationSignal, 0, this, null)
-
-        Toast.makeText(context,
-            "authentificate in startAuth",
-            Toast.LENGTH_LONG).show()
     }
 
     override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
-        Toast.makeText(context,
-            "Authentication error\n" + errString,
-            Toast.LENGTH_LONG).show()
+        informationMessage(context.getString(R.string.auth_error_message))
     }
 
     override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
-        Toast.makeText(context,
-            "Authentication succeeded. ",
-            Toast.LENGTH_LONG).show()
+        informationMessage(context.getString(R.string.aith_success_message))
     }
 
     override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
-        Toast.makeText(context,
-            "Authentication help\n" + helpString,
-            Toast.LENGTH_LONG).show()
+        informationMessage(context.getString(R.string.auth_help_message))
     }
 
     override fun onAuthenticationFailed() {
-        Toast.makeText(context,
-            "Authentication failed.",
-            Toast.LENGTH_LONG).show()
+        informationMessage(context.getString(R.string.auth_failed_message))
     }
+
+    private fun informationMessage(message: String) {
+        Toast.makeText(
+            context,
+            message,
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
 }
--- app/src/main/java/com/michalrola/fingerprintauth/MainActivity.kt
+++ app/src/main/java/com/michalrola/fingerprintauth/MainActivity.kt
@@ -3,23 +3,23 @@ package com.michalrola.fingerprintauth
 import android.Manifest
 import android.app.KeyguardManager
 import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.hardware.fingerprint.FingerprintManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import com.michalrola.fingerprintauth.authentication.EncryptionObject
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         val TAG = MainActivity::class.java.simpleName
+        private const val SECURE_KEY = "data.source.prefs.SECURE_KEY"
     }
 
     private lateinit var fingerprintManager: FingerprintManager
@@ -28,47 +28,41 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cryptoObjectDecrypt: FingerprintManager.CryptoObject
     private var encryptedMessage: String = "" //should have: message + separator + IV from first cipher
     private val separator = "-"
+    private lateinit var pref: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
-    private lateinit var encryptedTextView: TextView
-    private lateinit var decryptedTextView: TextView
-    private lateinit var encryptButton: Button
-    private lateinit var decryptButton: Button
-    private lateinit var listenerButton: Button
-    private lateinit var listenerButtonEnc: Button
+    //    private lateinit var encryptedTextView: TextView
+//    private lateinit var decryptedTextView: TextView
+//    private lateinit var encryptButton: Button
+//    private lateinit var decryptButton: Button
+//    private lateinit var listenerButton: Button
+//    private lateinit var listenerButtonEnc: Button
     private lateinit var pinEditText: EditText
 
     private val encryptionObject = EncryptionObject.newInstance()
-
-    private val SECURE_KEY = "data.source.prefs.SECURE_KEY"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        encryptedTextView = findViewById(R.id.encryptedTextView) as TextView
-        decryptedTextView = findViewById(R.id.decryptedTextView) as TextView
-        encryptButton = findViewById(R.id.encryptButton) as Button
-        decryptButton = findViewById(R.id.decryptButton) as Button
-        listenerButton = findViewById(R.id.listenerButton) as Button
-        listenerButtonEnc = findViewById(R.id.listenerButtonEnc) as Button
-        pinEditText = findViewById(R.id.PINexitText) as EditText
+//        encryptedTextView = findViewById(R.id.encryptedTextView) as TextView
+//        decryptedTextView = findViewById(R.id.decryptedTextView) as TextView
+//        encryptButton = findViewById(R.id.encryptButton) as Button
+//        decryptButton = findViewById(R.id.decryptButton) as Button
+//        listenerButton = findViewById(R.id.listenerButton) as Button
+//        listenerButtonEnc = findViewById(R.id.listenerButtonEnc) as Button
+        pinEditText = findViewById(R.id.pinEditText) as EditText
 
         keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
         fingerprintManager = getSystemService(Context.FINGERPRINT_SERVICE) as FingerprintManager
 
 
-        var pref = this.getSharedPreferences(
+        pref = this.getSharedPreferences(
             "com.michalrola.secure.pref",
             Context.MODE_PRIVATE
         )
 
-        var editor = pref.edit()
-
-//        val mBottomSheetDialog = BottomSheetDialog(this)
-//        val sheetView = this.layoutInflater.inflate(R.layout.fingerprint_dialog, null)
-//        mBottomSheetDialog.setContentView(sheetView)
-//        mBottomSheetDialog.show()
-
+        editor = pref.edit()
         checkFingerprint()
 
         val encryptedTextFromSharedPref =
@@ -79,33 +73,58 @@ class MainActivity : AppCompatActivity() {
 
 
         listenerButtonEnc.setOnClickListener {
-            try {
-//                mBottomSheetDialog.show()
-                cryptoObjectEncrypt = FingerprintManager.CryptoObject(encryptionObject.cipherForEncryption())
-                val fingerprintHandler = FingerprintHandler(this)
-                fingerprintHandler.startAuth(fingerprintManager, cryptoObjectEncrypt)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+            createFingerprintHandlerEnc()
         }
 
         encryptButton.setOnClickListener {
-            try {
-                encryptedMessage = encryptionObject.encrypt(
-                    encryptionObject.cipherEnc,
-                    pinEditText.text.toString().toByteArray(Charsets.UTF_8),
-                    separator
-                )
-                editor.putString(SECURE_KEY, encryptedMessage)
-                editor.apply()
-
-                encryptedTextView.text = encryptedMessage
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+            encryptMessage()
         }
         listenerButton.setOnClickListener {
+            createFingerprintHandlerDec()
+        }
 
+        decryptButton.setOnClickListener {
+            decryptMessage()
+        }
+    }
+
+    private fun decryptMessage() {
+        val mess = pref.getString(SECURE_KEY, null).split(separator)[0]
+        val decryptedData = encryptionObject.decrypt(
+            encryptionObject.cipherDec,
+            mess
+        )
+        decryptedTextView.text = decryptedData
+    }
+
+    private fun encryptMessage() {
+        try {
+            encryptedMessage = encryptionObject.encrypt(
+                encryptionObject.cipherEnc,
+                pinEditText.text.toString().toByteArray(Charsets.UTF_8),
+                separator
+            )
+            editor.putString(SECURE_KEY, encryptedMessage)
+            editor.apply()
+
+            encryptedTextView.text = encryptedMessage
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun createFingerprintHandlerEnc() {
+        try {
+            cryptoObjectEncrypt = FingerprintManager.CryptoObject(encryptionObject.cipherForEncryption())
+            val fingerprintHandler = FingerprintHandler(this)
+            fingerprintHandler.startAuth(fingerprintManager, cryptoObjectEncrypt)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun createFingerprintHandlerDec() {
+        try {
             cryptoObjectDecrypt = FingerprintManager.CryptoObject(
                 encryptionObject.cipherForDecryption(
                     pref.getString(SECURE_KEY, null).split(separator)[1].replace("\n", "")
@@ -113,21 +132,14 @@ class MainActivity : AppCompatActivity() {
             )
             val fingerprintHandler = FingerprintHandler(this)
             fingerprintHandler.startAuth(fingerprintManager, cryptoObjectDecrypt)
-        }
-
-        decryptButton.setOnClickListener {
-            val mess = pref.getString(SECURE_KEY, null).split(separator)[0]
-            val decryptedData = encryptionObject.decrypt(
-                encryptionObject.cipherDec,
-                mess
-            )
-            decryptedTextView.text = decryptedData
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
         }
     }
 
     private fun checkFingerprint() {
         if (!keyguardManager.isKeyguardSecure) {
-            Toast.makeText(this, "Lock screen security not enabled in Settings", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.fingerpint_not_entabled_message), Toast.LENGTH_SHORT).show()
             return
         }
 
@@ -136,7 +148,7 @@ class MainActivity : AppCompatActivity() {
         ) {
             Toast.makeText(
                 this,
-                "Fingerprint authentication permission not enabled",
+                getString(R.string.fingerpint_permissions_not_entabled_message),
                 Toast.LENGTH_LONG
             ).show()
             return
@@ -144,7 +156,7 @@ class MainActivity : AppCompatActivity() {
         if (!fingerprintManager.hasEnrolledFingerprints()) {
             Toast.makeText(
                 this,
-                "Register at least one fingerprint in Settings",
+                getString(R.string.fingerpint_not_registered_message),
                 Toast.LENGTH_LONG
             ).show()
             return
